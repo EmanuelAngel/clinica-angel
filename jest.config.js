d1f571a1acd2449aceede6535b4d2edc67daa536
EmanuelAngel <emanuelangelsbr@gmail.com>
2025-12-19 00:06:02 -0300

    test: delete coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -12,14 +12,6 @@ const config = {
     "!**/node_modules/**",
     "!**/vendor/**",
   ],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
   verbose: true,
 };
 
