ec99bb4130ee0e2cb7fdbaa290beea8be25c316a
EmanuelAngel <emanuelangelsbr@gmail.com>
2026-01-15 21:43:39 -0300

    feat(schedules): add controller and routes

---
+++ src/schedules/infrastructure/schedule.controller.js
@@ -0,0 +1,70 @@
+import z from "zod";
+import { services } from "../../_shared/infrastructure/services-container.js";
+import { validateCreateSchedule } from "./schedule.schemas.js";
+
+export class ScheduleController {
+  /**
+   *
+   * @param {import("express").Request} req
+   * @param {import("express").Response} res
+   */
+  async create(req, res) {
+    res.locals.view = "configure-schedule";
+
+    const [classifications, locations, professionals] = await Promise.all([
+      services.classificationService.findAll(),
+      services.locationService.findAll(),
+      services.professionalService.findAll(),
+    ]);
+
+    res.locals.viewData = {
+      classifications,
+      locations,
+      professionals,
+    };
+
+    const validationResult = await validateCreateSchedule(req.body);
+
+    if (!validationResult.success) {
+      res.status(422).render(res.locals.view, {
+        values: req.body,
+        errors: z.treeifyError(validationResult.error),
+        result: {
+          type: "failure",
+          message: "Revise el formulario e intente nuevamente.",
+        },
+      });
+
+      return;
+    }
+
+    const newConfigurationResult = await services.scheduleService.configure(
+      validationResult.data
+    );
+
+    if (newConfigurationResult.isErr()) {
+      res
+        .status(newConfigurationResult.error.statusCode)
+        .render(res.locals.view, {
+          values: req.body,
+          result: {
+            type: "failure",
+            message: newConfigurationResult.error.message,
+          },
+        });
+
+      return;
+    }
+
+    if (newConfigurationResult.isOk()) {
+      res.status(201).render(res.locals.view, {
+        result: {
+          type: "success",
+          message: "Agenda configurada exitosamente.",
+        },
+      });
+
+      return;
+    }
+  }
+}
---
+++ src/schedules/infrastructure/schedule.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { ScheduleController } from "./schedule.controller.js";
+import { auth } from "../../auth/infrastructure/auth.middleware.js";
+import { Roles } from "../../auth/domain/roles.js";
+
+export const scheduleRouter = Router();
+const scheduleController = new ScheduleController();
+
+scheduleRouter.post("/", auth(Roles.ADMIN), scheduleController.create);
