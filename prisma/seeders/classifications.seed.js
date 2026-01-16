4599f9de3b7298ec38c6fd05af44fb3cca6d37e1
EmanuelAngel <emanuelangelsbr@gmail.com>
2026-01-15 21:37:52 -0300

    feat(seed): add seeding for classifications and locations

---
+++ prisma/seeders/classifications.seed.js
@@ -0,0 +1,1 @@
+export const classifications = [{ name: "normal" }, { name: "vip" }];
---
+++ prisma/seeders/locations.seed.js
@@ -0,0 +1,17 @@
+export const locations = [
+  {
+    name: "Sede Central",
+    address: "Av. Principal 1232.",
+    phone: "239123789213",
+  },
+  {
+    name: "Sede Norte",
+    address: "Barrio Los Ceibos, entre Palermo y Ortiz.",
+    phone: "+54 266 7171717",
+  },
+  {
+    name: "Sede Sur",
+    address: "Barrio 108 viviendas, entre Lafinur y Malvinas.",
+    phone: "+54 266 626262",
+  },
+];
